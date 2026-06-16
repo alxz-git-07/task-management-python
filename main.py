9c16f059ce7463aa76ab1eb7ce23b05f0edd353f
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-06-16 08:23:58 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Import functions from task_manager.task_utils package
 from task_manager.task_utils import add_task,mark_task_as_complete,view_pending_tasks,calculate_progress
 None
-
+SHOW_MENU = False
 # Define the main function
 def main():
     while True:
-        print("Task Management System")
-        print("1. Add Task")
-        print("2. Mark Task as Complete")
-        print("3. View Pending Tasks")
-        print("4. View Progress")
-        print("5. Exit")
+        if SHOW_MENU:
+           print("Task Management System")
+           print("1. Add Task")
+           print("2. Mark Task as Complete")
+           print("3. View Pending Tasks")
+           print("4. View Progress")
+           print("5. Exit")
         choice = input("Enter your choice (1-5): ")
 
         if choice == "1":
