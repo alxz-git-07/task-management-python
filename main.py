de87a2f9f47078efe9c00542c31e024f01f227b9
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-06-16 12:26:49 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # Import functions from task_manager.task_utils package
 from task_manager.task_utils import add_task,mark_task_as_complete,view_pending_tasks,calculate_progress
 from task_manager.task_utils import tasks
-
+VIEW_MENU=False
 # Define the main function
 
 def main():
     while True:
-        print("Task Management System")
-        print("1. Add Task")
-        print("2. Mark Task as Complete")
-        print("3. View Pending Tasks")
-        print("4. View Progress")
-        print("5. Exit")
+        if VIEW_MENU:
+          print("Task Management System")
+          print("1. Add Task")
+          print("2. Mark Task as Complete")
+          print("3. View Pending Tasks")
+          print("4. View Progress")
+          print("5. Exit")
         choice = input('Enter your choice (1-5): ')
 
         if choice == "1":
@@ -21,7 +22,6 @@ def main():
                 due_date = input()
                 
                 if add_task(title,description,due_date):
-                    print(tasks)
                     break
                 print('Lets try that again\n')
             
